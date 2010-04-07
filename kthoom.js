952ca38b511b681d522b15c49025744dbc60830e
Jeff Schiller <codedread@gmail.com>
2010-04-07 10:27:34 -0500

    Fix Issue 1: support file picking and reading

--- kthoom.js
+++ kthoom.js
@@ -7,15 +7,55 @@
  *
  */
 
-if(!window.console) {
+/* Reference Documentation:
+
+  * File API (FileReader): http://www.w3.org/TR/FileAPI/
+
+*/
+
+if (!window.console) {
 	window.console = {};
 	window.console.log = function(str) {};
 	window.console.dir = function(str) {};
 }
 
-if(window.opera) {
+if (window.opera) {
 	window.console.log = function(str) {opera.postError(str);};
 	window.console.dir = function(str) {};
 }
 
-console.log("Hello, World!");
+// gets the element with the given id
+function getElem(id) {
+	if(document.documentElement.querySelector) {
+		// querySelector lookup
+		return document.body.querySelector('#'+id);
+	}	
+	// getElementById lookup
+	return document.getElementById(id);
+}
+
+// attempts to read the file that the user has chosen
+function getFile(evt) {
+	var inp = evt.target;
+	var filelist = inp.files;
+	if (filelist.length == 1) {
+		var reader = new FileReader();
+		reader.onloadend = function(e) {
+			// TODO: process e.target.result as a binary string
+			console.log("Done reading in file");
+		};
+		console.log("Reading in file '" + filelist[0].fileName + "'");
+		reader.readAsBinaryString(filelist[0]);
+	}
+}
+
+// attaches a change event listener to the file input control
+function init() {
+	if (!window.FileReader) {
+		alert("Sorry, kthoom will not work with your browser because it does not support the File API.  Please try kthoom with Firefox 3.6+.");
+	}
+	else {
+		var inp = getElem("filechooser");
+		inp.addEventListener("change", getFile, false);
+	}
+}
