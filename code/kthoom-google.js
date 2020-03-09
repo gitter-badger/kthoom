1609d2a15c02a4174dac27fbfd3b9c54d7d56db4
codedread <jeffschiller@google.com>
2020-03-09 14:01:59 -0700

    Rework Google OAuth dance.  Remove fetching Google API scripts from main HTML.

--- code/kthoom-google.js
+++ code/kthoom-google.js
@@ -7,6 +7,9 @@
  */
 /**
  * Code for handling file access through Google Drive.
+ *
+ * Ideally, we don't want any Google code to load unless the user clicks
+ * the Open menu item.
  */
 
 if (window.kthoom == undefined) {
@@ -16,75 +19,172 @@ if (window.kthoom == undefined) {
 let openMenu;
 
 function defineGoogleHooks() {
-  // TODO: Turn this script into a module and remove most things from window.kthoom.google.
-  window.kthoom.google = {
-    authed: false,
-    oathToken: undefined,
+  const SCOPE = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/drive.readonly',
+  ].join(' ');
 
-    boot: function() {
-      if (typeof gapi !== 'undefined') {
-        gapi.load('client', () => {
-          gapi.client.init({
-            'apiKey': window.kthoom.google.apiKey,
-            'clientId': window.kthoom.google.clientId,
-            'scope': [
-              'https://www.googleapis.com/auth/userinfo.email',
-              'https://www.googleapis.com/auth/userinfo.profile',
-              'https://www.googleapis.com/auth/drive'
-            ].join(' '),
-          }).then(() => {
-            kthoom.google.authorize(true, () => {
-              openMenu.showMenuItem('menu-open-google-drive', true);
-            });
+  // TODO: Turn this script into a module and remove most things from window.kthoom.google.
+  // TODO: Use ES2015 method definitions when we drop IE support?
+  window.kthoom.google = {
+    isBooted: false,
+    isSignedIn: false,
+    isAuthorized: false,
+    isReadyToCallAPIs: false,
+    oathToken: undefined,
+    authInstance: undefined,
+
+    boot: async function() {
+      if (typeof gapi === 'undefined') {
+        // Load the Google API script.
+        await new Promise((resolve, reject) => {
+          // If we cannot load the Google API script, then die.
+          const gScript = document.createElement('script');
+          gScript.setAttribute('async', 'async');
+          gScript.setAttribute('defer', 'defer');
+          gScript.onerror = err => reject(err);
+          gScript.addEventListener('load', () => resolve());
+          gScript.setAttribute('src', 'https://apis.google.com/js/api.js');
+          document.body.appendChild(gScript);
+        });
+
+        // Load initial API client and auth2 modules.
+        await new Promise((resolve, reject) => {
+          if (typeof gapi === 'undefined') {
+            reject('gapi was not defined');
+          }
+          gapi.load('client:auth2', {
+            callback: () => {
+              gapi.client.init({
+                'apiKey': kthoom.google.apiKey,
+                'clientId': kthoom.google.clientId,
+                'scope': SCOPE,
+                'discoveryDocs': [
+                  'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+                ],
+              }).then(() => {
+                kthoom.google.isBooted = true;
+
+                // See if we are already signed in and authorized.
+                const authInstance = gapi.auth2.getAuthInstance();
+                const currentUser = authInstance.currentUser.get();
+                authInstance.isSignedIn.listen(signedIn => {
+                  kthoom.google.isSignedIn = signedIn;
+                });
+                kthoom.google.authInstance = authInstance;
+                kthoom.google.isSignedIn = authInstance.isSignedIn.get();
+
+                const hasScopes = currentUser.hasGrantedScopes(SCOPE);
+                if (hasScopes) {
+                  const result = gapi.client.getToken();
+                  if (result && !result.error && result.access_token) {
+                    kthoom.google.isAuthorized = true;
+                    kthoom.google.oathToken = result.access_token;
+                    gapi.client.setApiKey(kthoom.google.oauthToken);
+                  }
+                }
+                resolve();
+              }, err => {
+                debugger;
+                reject(err);
+              });
+            },
+            onerror: err => reject(err),
           });
         });
       }
     },
 
-    authorize: function(immediate, callbackFn) {
-      // If we authenticated, then load the Drive and Picker APIs and after that,
-      // call the callback function.
+    authorize: async function() {
+      if (!kthoom.google.isBooted) {
+        await kthoom.google.boot();
+      }
+
+      // If signed in, but never were granted scopes or an OAuth token, sign out.
+      if (kthoom.google.isSignedIn && !kthoom.google.isAuthorized) {
+        await kthoom.authInstance.signOut();
+        // TODO: Should we call authInstance.disconnect() ?
+        kthoom.google.isSignedIn = false;
+      }
+
+      // If not signed in, then do it.
+      if (!kthoom.google.isSignedIn) {
+        await kthoom.google.authInstance.signIn();
+        kthoom.google.isSignedIn = true;
+      }
+
       const result = gapi.client.getToken();
-      if (result && !result.error) {
+      if (result && !result.error && result.access_token) {
+        kthoom.google.isAuthorized = true;
         kthoom.google.oathToken = result.access_token;
-        kthoom.google.authed = true;
-        gapi.client.load('drive', 'v2', function() {
-          gapi.load('picker', callbackFn);
-        });
+        gapi.client.setApiKey(kthoom.google.oauthToken);
       } else {
-        // Else, if we are not Google authenticated, and this was immediate, then
-        // just do the callback.  Otherwise, we've failed horribly so die.
-        if (immediate) {
-          callbackFn();
-        } else {
-          alert('There was a problem authenticating with Google.  ' +
-              'Please try again later.');
-        }
+        throw `Not authorized: ${result.error}`;
       }
     },
 
-    doDrive: function() {
-      if (!kthoom.google.authed) {
-        kthoom.google.authorize(false /* immediate */, kthoom.google.doDrive);
-      } else {
-        const docsView = new google.picker.DocsView();
-        docsView.setMode(google.picker.DocsViewMode.LIST);
-        docsView.setQuery('*.cbr|*.cbz|*.cbt');
-        const picker = new google.picker.PickerBuilder().
-            addView(docsView).
-            // Enable this feature when we can efficiently get downloadUrls
-            // for each file selected (right now we'd have to do drive.get
-            // calls for each file which is annoying the way we have set up
-            // library.allBooks).
-            //enableFeature(google.picker.Feature.MULTISELECT_ENABLED).
-            enableFeature(google.picker.Feature.NAV_HIDDEN).
-            setOAuthToken(kthoom.google.oathToken).
-            setDeveloperKey(kthoom.google.apiKey).
-            setAppId(kthoom.google.clientId).
-            setCallback(kthoom.google.pickerCallback).
-            build();
-        picker.setVisible(true);
+    loadAPILibs: async function() {
+      if (!kthoom.google.isBooted) {
+        await kthoom.google.boot();
       }
+
+      if (!kthoom.google.isAuthorized) {
+        await kthoom.google.authorize();
+      }
+
+      return new Promise((resolve, reject) => {
+        // Load the Drive and Picker APIs.
+        gapi.client.load('drive', 'v3', () => {
+          gapi.load('picker', {
+            callback: () => {
+              kthoom.google.isReadyToCallAPIs = true;
+              resolve();
+            },
+            onerror: err => reject(err),
+            timeout: 5000,
+            ontimeout: () => {
+              reject('gapi.load(picker) timed out');
+            },
+          });
+        }, err => {
+          reject(err);
+        })
+      });
+    },
+
+    doDrive: async function() {
+      // TODO: Show "Please wait" or a spinner while things get ready.
+
+      if (!kthoom.google.isBooted) {
+        await kthoom.google.boot();
+      }
+
+      if (!kthoom.google.isAuthorized) {
+        await kthoom.google.authorize();
+      }
+
+      if (!kthoom.google.isReadyToCallAPIs) {
+        await kthoom.google.loadAPILibs();
+      }
+
+      const docsView = new google.picker.DocsView();
+      docsView.setMode(google.picker.DocsViewMode.LIST);
+      docsView.setQuery('*.cbr|*.cbz|*.cbt');
+      const picker = new google.picker.PickerBuilder().
+          addView(docsView).
+          // Enable this feature when we can efficiently get downloadUrls
+          // for each file selected (right now we'd have to do drive.get
+          // calls for each file which is annoying the way we have set up
+          // library.allBooks).
+          //enableFeature(google.picker.Feature.MULTISELECT_ENABLED).
+          enableFeature(google.picker.Feature.NAV_HIDDEN).
+          setOAuthToken(kthoom.google.oathToken).
+          setDeveloperKey(kthoom.google.apiKey).
+          setAppId(kthoom.google.clientId).
+          setCallback(kthoom.google.pickerCallback).
+          build();
+      picker.setVisible(true);
     },
 
     pickerCallback : function(data) {
@@ -92,10 +192,11 @@ function defineGoogleHooks() {
         const fullSize = data.docs[0].sizeBytes;
         const gRequest = gapi.client.drive.files.get({
             'fileId': data.docs[0].id,
+            'fields': 'webContentLink',
         });
         gRequest.execute(function(response) {
           const bookName = data.docs[0].name;
-          const bookUrl = response.downloadUrl;
+          const bookUrl = response.webContentLink;
           // Try to download using fetch, otherwise use XHR.
           try {
             const myHeaders = new Headers();
@@ -159,25 +260,12 @@ function defineGoogleHooks() {
       throw 'No Open menu found in the kthoom app';
     }
 
-    // If we get here, we know the hosted kthoom instance has a Google API Key and we need to
-    // prepare to load and execute the Google API stuff and show the menu item.
-
+    // If we get here, we know the hosted kthoom instance has a Google API Key, so
+    // we can show the Open menu item.
     defineGoogleHooks();
-    window.kthoom.google.apiKey = gkey['apiKey'];
-    window.kthoom.google.clientId = gkey['clientId'];
-
-    // Load the Google API script.
-    await new Promise((resolve, reject) => {
-      // If we cannot load the Google API script, then die.
-      const gScript = document.createElement('script');
-      gScript.onerror = err => reject(err);
-      gScript.onload = () => resolve();
-      gScript.setAttribute('src', 'https://apis.google.com/js/platform.js');
-      document.body.appendChild(gScript);
-    });
-
-    window.kthoom.google.boot();
-
+    kthoom.google.apiKey = gkey['apiKey'];
+    kthoom.google.clientId = gkey['clientId'];
+    openMenu.showMenuItem('menu-open-google-drive', true);
   } catch (err) {
     // Die.
     console.warn(`Google integration was not found: ${err}`);
