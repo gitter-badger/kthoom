80e91db794dcac9867f2f699110e1aa3e9035a7e
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-03-27 02:19:28 -0500

    another one

--- code/book-viewer.js
+++ code/book-viewer.js
@@ -637,7 +637,7 @@ export class BookViewer {
           let page = getElem(`page${i+1}`).children;
           for(const pageElem  of page)
           {
-          pageElem.setAttribute("width", getElem(`page${i + 1}Image`).getAttribute("width")); //TODO:img.naturalWidth Do largest natural width of images or width to 100% above?
+          pageElem.setAttribute("width", getElem(`page${i + 1}Image`).getAttribute("width")); //TODO: img.naturalWidth Do largest natural width of images or width to 100% above?
           pageElem.setAttribute("height", img.naturalHeight); 
           pageElem.setAttribute("y", pt);
           
@@ -656,10 +656,11 @@ export class BookViewer {
           {
             pageElem.setAttribute("y", position ); 
           }
+          if(i+1 === that.currentBook_.getNumberOfPages()){
+            svgTop.setAttribute("height",position);
+          }
         }
-        if(i+1 === that.currentBook_.getNumberOfPages()){
-          svgTop.setAttribute("height",position);
-        }
+        
       }
           
         }
