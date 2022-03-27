f04f43147b320f51d420c858c361e54e76f0f4e6
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-03-27 02:07:10 -0500

    setwidth?

--- code/book-viewer.js
+++ code/book-viewer.js
@@ -637,7 +637,7 @@ export class BookViewer {
           let page = getElem(`page${i+1}`).children;
           for(const pageElem  of page)
           {
-          //pageElem.setAttribute("width", img.naturalWidth); //TODO: Do largest natural width of images or width to 100% above?
+          pageElem.setAttribute("width", getElem(`page${i + 1}Image`).getAttribute("width")); //TODO:img.naturalWidth Do largest natural width of images or width to 100% above?
           pageElem.setAttribute("height", img.naturalHeight); 
           pageElem.setAttribute("y", pt);
           
