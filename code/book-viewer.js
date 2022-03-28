83be10d85278f1ec5b01cd664c9f353b6367d13f
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-03-27 19:22:10 -0500

    remove comments

--- code/book-viewer.js
+++ code/book-viewer.js
@@ -508,133 +508,40 @@ export class BookViewer {
       this.currentPageNum_ + 1 : 0, page2);
     }
     else{
-      // //long-strip view.
+      //long-strip view.
       page1.style.display = '';
       page2.style.display = '';
       for(let i = 2; i < this.currentBook_.getNumberOfPages; i++){
         getElem(`page${i+1}`).style.display = '';
       }
-
-      // // TODO: Test this.
-      // // This is the dimensions before transformation.  They can go beyond the bv dimensions.
-      // let pw, ph, pl, pt;
-
-      // if (portraitMode) {
-      //   // Portrait, long-strip.
-      //   if (this.fitMode_ === FitMode.Width ||
-      //     (this.fitMode_ === FitMode.Best && bv.ar <= par)) {
-      //     // fit-width, long-strip.
-      //     // fit-best, long-strip, width maxed.
-      //     pw = bv.width;
-      //     ph = pw / par;
-      //     pl = bv.left;
-      //     if (par > bv.ar) { // not scrollable.
-      //       pt = roty - ph / 2;
-      //     } else{
-      //      // fit-width, scrollable.
-      //       pt = roty - bv.height / 2;
-      //       if (this.rotateTimes_ === 2) {
-      //         pt += bv.height - ph;
-            
-      //     }
-      //   }
-      //   } else {
-      //     // fit-height, long-strip.
-      //     // fit-best, long-strip, height maxed.
-      //     ph = bv.height;
-      //     pw = ph * par;
-      //     pt = bv.top;
-      //     if (par < bv.ar) { // not scrollable.
-      //       pl = rotx - pw / 2;
-      //     } else { // fit-height, scrollable.
-      //       pl = bv.left;
-      //       if (this.rotateTimes_ === 2) {
-      //         pl += bv.width - pw;
-      //       }
-      //     }
-      //   }
-
-      //   if (topw < pw) topw = pw;
-      //   if (toph < ph) toph = ph;
-        
-      // } else {
-      //   // Landscape, long-strip.
-      //   if (this.fitMode_ === FitMode.Width ||
-      //     (this.fitMode_ === FitMode.Best && par > (1 / bv.ar))) {
-      //     // fit-best, long-strip, width-maxed.
-      //     // fit-width, long-strip.
-      //     pw = bv.height;
-      //     ph = pw / par;
-      //     pl = rotx - pw / 2;
-      //     if (par > (1 / bv.ar)) { // not scrollable.
-      //       pt = roty - ph / 2;
-      //     } else {
-      //    // fit-width, scrollable.
-      //       pt = roty - bv.width / 2;
-      //       if (this.rotateTimes_ === 1) {
-      //         pt += bv.width - ph;
-      //       }
-      //     }
-      //   } else {
-      //     // fit-best, long-strip, height-maxed.
-      //     // fit-height, long-strip.
-      //     ph = bv.width;
-      //     pw = ph * par;
-      //     pt = roty - ph / 2;
-      //     if (par > (1 / bv.ar)) { // not scrollable.
-      //       pt = roty - ph / 2;
-      //     } else {
-      //     // fit-height, scrollable.
-      //       pl = rotx - bv.height / 2;
-      //       if (this.rotateTimes_ === 3) {
-      //         pl += bv.height - pw;
-            
-      //     }
-      //   }
-      //   }
-
-      //   if (topw < ph) topw = ph;
-      //   if (toph < pw) toph = pw;
-      // } // Landscape
- 
-   
-    
  
      //Now size the page elements.
       for (const pageElem of page1Elems) {
         pageElem.setAttribute("style","-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;");
-        // pageElem.setAttribute('x', 0);
-        // pageElem.setAttribute('y', );
+    
         pageElem.setAttribute("width", window.innerWidth);
-        //  pageElem.setAttribute("height", '?');
-        //  pageElem.setAttribute("display","none");
+     
       }
-       //pt += ph;
        for (const pageElem of page2Elems) {
         pageElem.setAttribute("style","-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;");
 
-        //  pageElem.setAttribute('x', '?');
-         
+        
           pageElem.setAttribute('y',getElem("page1Image").getBBox().height);
           pageElem.setAttribute("width", window.innerWidth);
-        //  pageElem.setAttribute("height", '?');
-        //  pageElem.setAttribute("display","none");
+        
        }
        let y = parseFloat(getElem("page2Image").getBBox().y) + parseFloat(getElem("page2Image").getBBox().height); //TODO: GetElem or from arrays
        let q = 0;
        for(const page of pageN){
-        // pt += ph;
         if(q > 0){
          y += getElem(`page${q+1}Image`).getBBox().height;
        }
          
          for(const pageElem of page ){
           pageElem.setAttribute("style","-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;");
-          // pageElem.setAttribute("x", '?');
            pageElem.setAttribute("y", y);
            pageElem.setAttribute("width", window.innerWidth);
-           //pageElem.setAttribute("height", 'auto');
-          // pageElem.setAttribute("display","none");
+         
           
          }
          toph = y; 
@@ -642,46 +549,11 @@ export class BookViewer {
        }
        for (let i = 0; i < this.currentBook_.getNumberOfPages(); i++){
           this.showPageInViewer_(i,getElem(`page${i+1}`)); //TODO: add Promise.all()
-          // let that = this;
-          // setTimeout(function(){
-            
-          // const thePage = that.currentBook_.getPage(i);
-
-          //     let position = 0;
-          //     let img = new Image();
-          //     img.onload = function(){
-          //     let page = getElem(`page${i+1}`).children;
-          //     if(i != 0)
-          //     {
-          //     position = parseFloat(getElem(`page${i}Image`).getAttribute("y")) + parseFloat(getElem(`page${i}Image`).getAttribute("height"));
-          //     }
-          //     for(const pageElem of page)
-          //     {
-                
-          //     // pageElem.setAttribute("width",pw)
-          //      pageElem.setAttribute("height", img.naturalHeight);    
-          //       pageElem.setAttribute("x", 0 );
-          //       pageElem.setAttribute("y", position ); 
-          //       pageElem.setAttribute("display","contents");
-                
-          //     }
-          //      if(i+1 === that.currentBook_.getNumberOfPages()){
-          //        svgTop.setAttribute("height",position);
-          //      }
-          //   };
-          //   img.src = thePage.getURI();
-
-          //   this.showPageInViewer_(i,getElem(`page${i+1}`)); //TODO: add Promise.all()
-          // },i+200);
-
-
-
-       // this.showPageInViewer_(i,getElem(`page${i+1}`)); //TODO: add Promise.all()
-        
+           
       
       }
        
-      //toph *= this.currentBook_.getNumberOfPages();
+     
     }
 
     // Rotate the book viewer viewport.
