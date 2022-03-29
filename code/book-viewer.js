d46133b67db966280f5b93888f9379fccb329a33
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-03-29 00:50:55 -0500

    first in

--- code/book-viewer.js
+++ code/book-viewer.js
@@ -648,7 +648,7 @@ export class BookViewer {
     svgTop.setAttribute('height', toph );
     svgTop.style.display="none";
     getElem("page2Image").scrollIntoView();
-    if (!this.checkInView(svgTop,getElem("page2Image"),true) && this.getNumPagesInViewer() === 3){
+    if (this.getNumPagesInViewer() === 3 && !this.checkInView(svgTop,getElem("page2Image"),true) ){
       let side = 0;
       for (let i = 0; i < 2; i++){ 
         const tempTr = `translate(${rotx}, ${roty}) rotate(${(this.getRotateTimes() -1 - i) * 90}) translate(${-rotx}, ${-roty})`;
