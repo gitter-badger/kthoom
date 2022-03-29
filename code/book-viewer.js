9e347d044c5bf705770b16c9d65ea6cf26445999
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-03-28 19:08:47 -0500

    extra brac

--- code/book-viewer.js
+++ code/book-viewer.js
@@ -527,12 +527,10 @@ export class BookViewer {
         pageElem.removeAttribute("width");
        
         pageElem.setAttribute("style","-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;");
-       
-      
+     
             pageElem.setAttribute("width", window.innerWidth);
 
-          
-          }
+           
 
      
       }
