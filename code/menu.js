8f38fa155a5d95ba45ae7427c5032db192b77154
Jeff Schiller <codedread@gmail.com>
2022-04-24 08:19:18 -0700

    Move Menu to using private fields

--- code/menu.js
+++ code/menu.js
@@ -119,20 +119,22 @@ export class MenuEvent extends Event {
  * interaction.  Clients create menus and add event listeners for when menu items are selected.
  */
 export class Menu extends EventTarget {
+  /** @type {HTMLDivElement} */
+  #dom = undefined;
+
+  /** @type {Map<string, Menu>} */
+  #subMenuMap = new Map();
+
+  /** @private {Menu} */
+  #openedSubMenu = null;
+
   /**
    * @param {HTMLTemplateElement} templateEl 
    */
   constructor(templateEl) {
     super();
 
-    this.dom_ = undefined;
-    this.createDom_(templateEl);
-
-    /** @private {Map<string, Menu>} */
-    this.subMenuMap_ = new Map();
-
-    /** @private {Menu} */
-    this.openedSubMenu_ = null;
+    this.#createDom(templateEl);
   }
 
   /**
@@ -140,13 +142,13 @@ export class Menu extends EventTarget {
    * @param {Menu} subMenu
    */
   addSubMenu(menuItemId, subMenu) {
-    assert(!Array.from(this.subMenuMap_.values()).includes(subMenu), 'Submenu already part of this menu.');
-    assert(!this.subMenuMap_.has(menuItemId), `Menu already has a submenu mapped for ${menuItemId}`);
-    const menuEl = this.dom_.firstElementChild;
+    assert(!Array.from(this.#subMenuMap.values()).includes(subMenu), 'Submenu already part of this menu.');
+    assert(!this.#subMenuMap.has(menuItemId), `Menu already has a submenu mapped for ${menuItemId}`);
+    const menuEl = this.#dom.firstElementChild;
     const menuItem = menuEl.querySelectorAll(`[id="${menuItemId}"][role="menuitem"]`);
     assert(!!menuItem, `Menu item "${menuItemId} not found in menu`);
 
-    this.subMenuMap_.set(menuItemId, subMenu);
+    this.#subMenuMap.set(menuItemId, subMenu);
   }
 
   /**
@@ -154,14 +156,14 @@ export class Menu extends EventTarget {
    */
   close() {
     assert(openMenus.has(this), 'Menu was already closed!');
-    if (this.openedSubMenu_) {
-      if (this.openedSubMenu_.isOpen()) {
-        this.openedSubMenu_.close();
+    if (this.#openedSubMenu) {
+      if (this.#openedSubMenu.isOpen()) {
+        this.#openedSubMenu.close();
       }
-      this.openedSubMenu_ = null;
+      this.#openedSubMenu = null;
     }
 
-    this.dom_.style = MENU_CLOSED_STYLE;
+    this.#dom.style = MENU_CLOSED_STYLE;
     openMenus.delete(this);
     this.dispatchEvent(new MenuEvent(this, MenuEventType.CLOSE));
 
@@ -171,39 +173,184 @@ export class Menu extends EventTarget {
   }
 
   /**
-   * @param {HTMLTemplateElement} templateEl
-   * @private
+   * @param {KeyboardEvent} evt
+   * @returns {boolean} True if the event was handled.
    */
-  createDom_(templateEl) {
-    assert(!this.dom_, 'DOM for Menu was already created when createDom_() was called');
+  handleKeyEvent(evt) {
+    if (!this.isOpen()) {
+      return false;
+    }
 
-    this.dom_ = document.createElement('div');
-    this.dom_.style = MENU_CLOSED_STYLE;
-    this.dom_.appendChild(document.importNode(templateEl.content, true));
+    const code = evt.keyCode;
+
+    const closeKeys = [Key.ESCAPE, Key.TAB, Key.LEFT];
+    if (this.#openedSubMenu && !closeKeys.includes(code)) {
+      return this.#openedSubMenu.handleKeyEvent(evt);
+    }
+
+    if (closeKeys.includes(code)) {
+      if (this.#openedSubMenu) {
+        if (this.#openedSubMenu.isOpen()) {
+          this.#openedSubMenu.close();
+        }
+        // Find the appropriate menu item and focus is.
+        for (const menuItemId of this.#subMenuMap.keys()) {
+          const subMenu = this.#subMenuMap.get(menuItemId);
+          if (subMenu === this.#openedSubMenu) {
+            const menuItem = getElem(menuItemId);
+            menuItem.setAttribute('aria-expanded', 'false');
+            menuItem.focus();
+            break;
+          }
+        }
+        this.#openedSubMenu = null;
+      } else {
+        this.close();
+      }
+      return true;
+    }
+
+    switch (code) {
+      case Key.UP:
+        // TODO
+        if (Params.longStripView) {
+          evt.preventDefault();
+        }
+        evt.stopPropagation();
+        this.#focusMenuItem(-1);
+        return true;
+      case Key.DOWN:
+        // TODO
+        if (Params.longStripView) {
+          evt.preventDefault();
+        }
+        evt.stopPropagation();
+        this.#focusMenuItem(1);
+        return true;
+      case Key.RIGHT:
+        const currentlyFocusedMenuItem = document.activeElement;
+        const menuItemId = currentlyFocusedMenuItem.id;
+        if (this.#subMenuMap.has(menuItemId)) {
+          const subMenu = this.#subMenuMap.get(menuItemId);
+          subMenu.open(currentlyFocusedMenuItem.offsetWidth, currentlyFocusedMenuItem.offsetTop);
+          this.#openedSubMenu = subMenu;
+          return true;
+        }
+        break;
+      case Key.ENTER:
+        // We need to return true so that the event propagates up to the browser with the menu item
+        // still focused so that a 'click' event occurs.  The click handler closes the menu.
+        return true;
+    }
+    // Otherwise, the menu did not process the event and something above us should.
+    this.close();
+    return false;
+  }
+
+  /** @returns {boolean} */
+  isOpen() {
+    return !(this.#dom.style.display === 'none');
+  }
+
+  /**
+   * @param {number} left The left px value.
+   * @param {number} top The top px value.
+   */
+  open(left = 0, top = 0) {
+    assert(!openMenus.has(this), 'Menu was already open!');
+    overlay.style = MENU_OVERLAY_OPEN_STYLE;
+    const style = MENU_OPEN_STYLE + `left:${left}px;top:${top}px;`;
+    this.#dom.style = style;
+    openMenus.add(this);
+    this.dispatchEvent(new MenuEvent(this, MenuEventType.OPEN));
+
+    const menuEl = this.#dom.firstElementChild;
+    const firstMenuElem = menuEl.querySelector('[role="menuitem"]:not([disabled="true"])');
+    firstMenuElem.focus();
+  }
+
+  /**
+   * Sets the selected state on a menu item.  Assumes the menu item has a span with the
+   * menuCheckmark class.
+   * @param {string} itemId 
+   * @param {boolean} selected True to select, false to de-select.
+   */
+  setMenuItemSelected(itemId, selected) {
+    const menuEl = this.#dom.firstElementChild;
+    const menuItemCheckmark = menuEl.querySelector(`[id="${itemId}"][role="menuitem"] .menuCheckmark`);
+    assert(!!menuItemCheckmark, `Could not find checkmark span for menu item ${itemId}`);
+
+    menuItemCheckmark.innerHTML = selected ? '✔︎' : '&nbsp;';
+  }
+
+  /**
+   * Gets the selected state on a menu item.  Assumes the menu item has a span with the
+   * menuCheckmark class.
+   * @param {string} itemId 
+   * @returns {boolean} True if the menu item is selected
+   */
+  getMenuItemSelected(itemId) {
+    const menuEl = this.#dom.firstElementChild;
+    const menuItemCheckmark = menuEl.querySelector(`[id="${itemId}"][role="menuitem"] .menuCheckmark`);
+    assert(!!menuItemCheckmark, `Could not find checkmark span for menu item ${itemId}`);
+    let selected = false;
+    if (menuItemCheckmark.innerHTML === '✔︎' ) {
+      selected = true;
+    } else if(menuItemCheckmark.innerHTML === '&nbsp;') {
+      selected = false;
+    }    
+    return selected;
+  }
+
+  /**
+   * @param {string} itemId 
+   * @param {boolean} show True to show, false to hide.
+   */
+  showMenuItem(itemId, show) {
+    const menuEl = this.#dom.firstElementChild;
+    const menuItem = menuEl.querySelector(`[id="${itemId}"][role="menuitem"]`);
+    assert(!!menuItem, `Could not find menu item ${itemId}`);
+    menuItem.style.display = show ? '' : 'none';
+    if (show) {
+      menuItem.removeAttribute('disabled');
+    } else {
+      menuItem.setAttribute('disabled', 'true');
+    }
+  }
+
+  /**
+   * @param {HTMLTemplateElement} templateEl
+   */
+  #createDom(templateEl) {
+    assert(!this.#dom, 'DOM for Menu was already created when #createDom() was called');
+
+    this.#dom = document.createElement('div');
+    this.#dom.style = MENU_CLOSED_STYLE;
+    this.#dom.appendChild(document.importNode(templateEl.content, true));
 
     // TODO: Do some validation on the DOM here.
-    getElem(MENU_CONTAINER).appendChild(this.dom_);
+    getElem(MENU_CONTAINER).appendChild(this.#dom);
 
     // Add all click listeners here.
-    const menuEl = this.dom_.firstElementChild;
+    const menuEl = this.#dom.firstElementChild;
     for (const menuItem of menuEl.querySelectorAll('[role="menuitem"]')) {
       menuItem.addEventListener('click', evt => {
         const menuItemId = menuItem.id;
         let previouslyOpenedSubMenu = null;
-        if (this.openedSubMenu_) {
-          if (this.openedSubMenu_.isOpen()) {
-            previouslyOpenedSubMenu = this.openedSubMenu_;
-            this.openedSubMenu_.close();
+        if (this.#openedSubMenu) {
+          if (this.#openedSubMenu.isOpen()) {
+            previouslyOpenedSubMenu = this.#openedSubMenu;
+            this.#openedSubMenu.close();
           }
-          this.openedSubMenu_ = null;
+          this.#openedSubMenu = null;
         }
-        if (this.subMenuMap_.has(menuItemId)) {
-          const maybeOpenSubMenu = this.subMenuMap_.get(menuItemId);
+        if (this.#subMenuMap.has(menuItemId)) {
+          const maybeOpenSubMenu = this.#subMenuMap.get(menuItemId);
           const menuItemEl = evt.target;
           if (maybeOpenSubMenu !== previouslyOpenedSubMenu) {
             menuItemEl.setAttribute('aria-expanded', 'true');
-            this.openedSubMenu_ = maybeOpenSubMenu;
-            this.openedSubMenu_.open(menuItemEl.offsetWidth, menuItemEl.offsetTop);
+            this.#openedSubMenu = maybeOpenSubMenu;
+            this.#openedSubMenu.open(menuItemEl.offsetWidth, menuItemEl.offsetTop);
           } else {
             menuItemEl.setAttribute('aria-expanded', 'false');
             menuItemEl.focus();
@@ -220,10 +367,9 @@ export class Menu extends EventTarget {
   /**
    * Assumes the menu is open.  Moves the menu item focus.
    * @param {Number} delta Can be negative (up) or positive (down)
-   * @private
    */
-  focusMenuItem_(delta = 1) {
-    const menuEl = this.dom_.firstElementChild;
+  #focusMenuItem(delta = 1) {
+    const menuEl = this.#dom.firstElementChild;
     const menuItems = menuEl.querySelectorAll('[role="menuitem"]:not([disabled="true"])');
     const numMenuItems = menuItems.length;
     const currentlyFocusedMenuItem = document.activeElement;
@@ -249,151 +395,5 @@ export class Menu extends EventTarget {
 
     const newlySelectedMenuItem = menuItems.item(i);
     newlySelectedMenuItem.focus();
-  }
-
-  /**
-   * @param {KeyboardEvent} evt
-   * @returns {boolean} True if the event was handled.
-   */
-  handleKeyEvent(evt) {
-    if (!this.isOpen()) {
-      return false;
-    }
-
-    const code = evt.keyCode;
-
-    const closeKeys = [Key.ESCAPE, Key.TAB, Key.LEFT];
-    if (this.openedSubMenu_ && !closeKeys.includes(code)) {
-      return this.openedSubMenu_.handleKeyEvent(evt);
-    }
-
-    if (closeKeys.includes(code)) {
-      if (this.openedSubMenu_) {
-        if (this.openedSubMenu_.isOpen()) {
-          this.openedSubMenu_.close();
-        }
-        // Find the appropriate menu item and focus is.
-        for (const menuItemId of this.subMenuMap_.keys()) {
-          const subMenu = this.subMenuMap_.get(menuItemId);
-          if (subMenu === this.openedSubMenu_) {
-            const menuItem = getElem(menuItemId);
-            menuItem.setAttribute('aria-expanded', 'false');
-            menuItem.focus();
-            break;
-          }
-        }
-        this.openedSubMenu_ = null;
-      } else {
-        this.close();
-      }
-      return true;
-    }
-
-    switch (code) {
-      case Key.UP:
-        // TODO
-        if (Params.longStripView) {
-          evt.preventDefault();
-        }
-        evt.stopPropagation();
-        this.focusMenuItem_(-1);
-        return true;
-      case Key.DOWN:
-        // TODO
-        if (Params.longStripView) {
-          evt.preventDefault();
-        }
-        evt.stopPropagation();
-        this.focusMenuItem_(1);
-        return true;
-      case Key.RIGHT:
-        const currentlyFocusedMenuItem = document.activeElement;
-        const menuItemId = currentlyFocusedMenuItem.id;
-        if (this.subMenuMap_.has(menuItemId)) {
-          const subMenu = this.subMenuMap_.get(menuItemId);
-          subMenu.open(currentlyFocusedMenuItem.offsetWidth, currentlyFocusedMenuItem.offsetTop);
-          this.openedSubMenu_ = subMenu;
-          return true;
-        }
-        break;
-      case Key.ENTER:
-        // We need to return true so that the event propagates up to the browser with the menu item
-        // still focused so that a 'click' event occurs.  The click handler closes the menu.
-        return true;
-    }
-    // Otherwise, the menu did not process the event and something above us should.
-    this.close();
-    return false;
-  }
-
-  /** @returns {boolean} */
-  isOpen() {
-    return !(this.dom_.style.display === 'none');
-  }
-
-  /**
-   * @param {number} left The left px value.
-   * @param {number} top The top px value.
-   */
-  open(left = 0, top = 0) {
-    assert(!openMenus.has(this), 'Menu was already open!');
-    overlay.style = MENU_OVERLAY_OPEN_STYLE;
-    const style = MENU_OPEN_STYLE + `left:${left}px;top:${top}px;`;
-    this.dom_.style = style;
-    openMenus.add(this);
-    this.dispatchEvent(new MenuEvent(this, MenuEventType.OPEN));
-
-    const menuEl = this.dom_.firstElementChild;
-    const firstMenuElem = menuEl.querySelector('[role="menuitem"]:not([disabled="true"])');
-    firstMenuElem.focus();
-  }
-
-  /**
-   * Sets the selected state on a menu item.  Assumes the menu item has a span with the
-   * menuCheckmark class.
-   * @param {string} itemId 
-   * @param {boolean} selected True to select, false to de-select.
-   */
-  setMenuItemSelected(itemId, selected) {
-    const menuEl = this.dom_.firstElementChild;
-    const menuItemCheckmark = menuEl.querySelector(`[id="${itemId}"][role="menuitem"] .menuCheckmark`);
-    assert(!!menuItemCheckmark, `Could not find checkmark span for menu item ${itemId}`);
-
-    menuItemCheckmark.innerHTML = selected ? '✔︎' : '&nbsp;';
-  }
-
-  /**
-   * Gets the selected state on a menu item.  Assumes the menu item has a span with the
-   * menuCheckmark class.
-   * @param {string} itemId 
-   * @returns {boolean} True if the menu item is selected
-   */
-  getMenuItemSelected(itemId) {
-    const menuEl = this.dom_.firstElementChild;
-    const menuItemCheckmark = menuEl.querySelector(`[id="${itemId}"][role="menuitem"] .menuCheckmark`);
-    assert(!!menuItemCheckmark, `Could not find checkmark span for menu item ${itemId}`);
-    let selected = false;
-    if (menuItemCheckmark.innerHTML === '✔︎' ) {
-      selected = true;
-    } else if(menuItemCheckmark.innerHTML === '&nbsp;') {
-      selected = false;
-    }    
-    return selected;
-  }
-
-  /**
-   * @param {string} itemId 
-   * @param {boolean} show True to show, false to hide.
-   */
-  showMenuItem(itemId, show) {
-    const menuEl = this.dom_.firstElementChild;
-    const menuItem = menuEl.querySelector(`[id="${itemId}"][role="menuitem"]`);
-    assert(!!menuItem, `Could not find menu item ${itemId}`);
-    menuItem.style.display = show ? '' : 'none';
-    if (show) {
-      menuItem.removeAttribute('disabled');
-    } else {
-      menuItem.setAttribute('disabled', 'true');
-    }
   }
 }
