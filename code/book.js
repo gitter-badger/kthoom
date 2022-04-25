3f06aec285ab538496b6f6ed386166fb65da1e3e
Jeff Schiller <codedread@gmail.com>
2022-04-25 12:23:34 -0700

    Update Book to use private fields/methods

--- code/book.js
+++ code/book.js
@@ -55,6 +55,68 @@ export class BookContainer {
  */
 export class Book extends EventTarget {
   /**
+   * The name of the book (shown in the Reading Stack).
+   * @type {String}
+   */
+  #name;
+
+  /**
+   * The optional URI of the book (not set for a book loaded from the file system).
+   * @type {String}
+   */
+  #uri;
+
+  /**
+   * The File object of the book.
+   * @type {File}
+   */
+  #file;
+
+  /**
+   * The optional FileSystemFileHandle of the book (not set for book loaded from a URI).
+   * @type {FileSystemFileHandle}
+   */
+  #fileHandle;
+
+  /**
+   * @type {BookContainer}
+   */
+  #bookContainer;
+
+  /** @type {boolean} */
+  #needsLoading = true;
+
+  /** @type {boolean} */
+  #finishedBinding = false;
+
+  /** @type {boolean} */
+  #finishedLoading = false;
+
+  /**
+   * The total known number of pages.
+   * @type {number}
+   */
+  #totalPages = 0;
+
+  /**
+   * @type {BookBinder}
+   */
+  #bookBinder = null;
+
+  /** @type {Array<Page>} */
+  #pages = [];
+
+  /** @type {BookMetadata} */
+  #bookMetadata = null;
+
+  /**
+   * A reference to the ArrayBuffer is kept to let the user easily download a copy.
+   * This array buffer is only valid once the book has fully loaded.
+   * @type {ArrayBuffer}
+   */
+  #arrayBuffer = null;
+
+  /**
    * @param {string} name
    * @param {string|File|FileSystemFileHandle} uriOrFileHandle For files loaded via URI, this param
    *    contains the URI. For files loaded via a file input element, this contains the File object,
@@ -64,89 +126,11 @@ export class Book extends EventTarget {
   constructor(name, uriOrFileHandle = undefined, bookContainer = undefined) {
     super();
 
-    /**
-     * The name of the book (shown in the Reading Stack).
-     * @private
-     * @type {String}
-     */
-    this.name_ = name;
-
-    /**
-     * The optional URI of the book (not set for a book loaded from the file system).
-     * @private
-     * @type {String}
-     */
-    this.uri_ = typeof(uriOrFileHandle) === 'string' ? uriOrFileHandle : undefined;
-
-    /**
-     * The File object of the book.
-     * @private
-     * @type {File}
-     */
-    this.file_ = (uriOrFileHandle instanceof File) ? uriOrFileHandle : undefined;
-
-    /**
-     * The optional FileSystemFileHandle of the book (not set for book loaded from a URI).
-     * @private
-     * @type {FileSystemFileHandle}
-     */
-    this.fileHandle_ = (!this.uri_ && !this.file_) ? uriOrFileHandle : undefined;
-
-    /**
-     * @private
-     * @type {BookContainer}
-     */
-    this.bookContainer_ = bookContainer;
-
-    /**
-     * @private
-     * @type {boolean}
-     */
-    this.needsLoading_ = true;
-
-    /**
-     * @private
-     * @type {boolean}
-     */
-     this.finishedBinding_ = false;
-
-    /**
-     * @private
-     * @type {boolean}
-     */
-    this.finishedLoading_ = false;
-
-    /**
-     * The total known number of pages.
-     * @private
-     * @type {number}
-     */
-    this.totalPages_ = 0;
-
-    /**
-     * @private
-     * @type {BookBinder}
-     */
-    this.bookBinder_ = null;
-
-    /**
-     * @private
-     * @type {Array<Page>}
-     */
-    this.pages_ = [];
-
-    /**
-     * @private
-     * @type {BookMetadata}
-     */
-    this.bookMetadata_ = null;
-
-    /**
-     * A reference to the ArrayBuffer is kept to let the user easily download a copy.
-     * This array buffer is only valid once the book has fully loaded.
-     * @private {ArrayBuffer}
-     */
-    this.arrayBuffer_ = null;
+    this.#name = name;
+    this.#uri = typeof(uriOrFileHandle) === 'string' ? uriOrFileHandle : undefined;
+    this.#file = (uriOrFileHandle instanceof File) ? uriOrFileHandle : undefined;
+    this.#fileHandle = (!this.#uri && !this.#file) ? uriOrFileHandle : undefined;
+    this.#bookContainer = bookContainer;
   }
 
   /**
@@ -154,68 +138,68 @@ export class Book extends EventTarget {
    * @param {ArrayBuffer} appendBuffer
    */
   appendBytes(appendBuffer) {
-    let newBuffer = new Uint8Array(this.arrayBuffer_.length + appendBuffer.length);
-    newBuffer.set(this.arrayBuffer_, 0);
-    newBuffer.set(appendBuffer, this.arrayBuffer_.length);
-    this.arrayBuffer_ = newBuffer;
+    let newBuffer = new Uint8Array(this.#arrayBuffer.length + appendBuffer.length);
+    newBuffer.set(this.#arrayBuffer, 0);
+    newBuffer.set(appendBuffer, this.#arrayBuffer.length);
+    this.#arrayBuffer = newBuffer;
   }
 
   /** @returns {Promise<ArrayBuffer>} */
   getArrayBuffer() {
-    return this.arrayBuffer_;
+    return this.#arrayBuffer;
   }
 
   /** @returns {BookContainer} */
-  getContainer() { return this.bookContainer_; }
+  getContainer() { return this.#bookContainer; }
 
   /** @returns {FileSystemFileHandle} */
-  getFileSystemHandle() { return this.fileHandle_; }
+  getFileSystemHandle() { return this.#fileHandle; }
 
   /** @returns {BookMetadata} */
-  getMetadata() { return this.bookMetadata_; }
+  getMetadata() { return this.#bookMetadata; }
 
   /** @returns {string} */
   getMIMEType() {
-    if (!this.bookBinder_) {
+    if (!this.#bookBinder) {
       throw 'Cannot call getMIMEType() without a BookBinder';
     }
-    return this.bookBinder_.getMIMEType();
+    return this.#bookBinder.getMIMEType();
   }
 
-  getName() { return this.name_; }
+  getName() { return this.#name; }
   getLoadingPercentage() {
-    if (!this.bookBinder_) return 0;
-    return this.bookBinder_.getLoadingPercentage();
+    if (!this.#bookBinder) return 0;
+    return this.#bookBinder.getLoadingPercentage();
   }
   getUnarchivingPercentage() {
-    if (!this.bookBinder_) return 0;
-    return this.bookBinder_.getUnarchivingPercentage();
+    if (!this.#bookBinder) return 0;
+    return this.#bookBinder.getUnarchivingPercentage();
   }
   getLayoutPercentage() {
-    if (!this.bookBinder_) return 0;
-    return this.bookBinder_.getLayoutPercentage();
+    if (!this.#bookBinder) return 0;
+    return this.#bookBinder.getLayoutPercentage();
   }
-  getNumberOfPages() { return this.totalPages_; }
-  getNumberOfPagesReady() { return this.pages_.length; }
+  getNumberOfPages() { return this.#totalPages; }
+  getNumberOfPagesReady() { return this.#pages.length; }
 
   /**
    * @param {number} i A number from 0 to (num_pages - 1).
    * @returns {Page}
    */
   getPage(i) {
-    // TODO: This is a bug in the unarchivers.  The only time totalPages_ is set is
+    // TODO: This is a bug in the unarchivers.  The only time #totalPages is set is
     // upon getting a UnarchiveEventType.PROGRESS which has the total number of files.
     // In some books, we get an EXTRACT event before we get the first PROGRESS event.
-    const numPages = this.totalPages_ || this.pages_.length;
+    const numPages = this.#totalPages || this.#pages.length;
     if (i < 0 || i >= numPages) {
       return null;
     }
-    return this.pages_[i];
+    return this.#pages[i];
   }
 
   /** @returns {string} */
   getUri() {
-    return this.uri_;
+    return this.#uri;
   }
 
   /**
@@ -224,7 +208,7 @@ export class Book extends EventTarget {
    * @returns {boolean}
    */
   isFinishedBinding() {
-    return this.finishedBinding_;
+    return this.#finishedBinding;
   }
 
   /**
@@ -232,7 +216,7 @@ export class Book extends EventTarget {
    * @returns {boolean}
    */
   isFinishedLoading() {
-    return this.finishedLoading_;
+    return this.#finishedLoading;
   }
 
   /**
@@ -240,9 +224,9 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>}
    */
   async load() {
-    if (this.uri_) {
+    if (this.#uri) {
       return this.loadFromXhr();
-    } else if (this.file_ || this.fileHandle_) {
+    } else if (this.#file || this.#fileHandle) {
       return this.loadFromFile();
     }
     throw 'Could not load Book: no uri or File or FileHandle';
@@ -256,37 +240,37 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>} A Promise that returns this book when all bytes have been fed to it.
    */
   loadFromXhr(expectedSize = -1, headerMap = {}) {
-    if (!this.needsLoading_) {
+    if (!this.#needsLoading) {
       throw 'Cannot try to load via XHR when the Book is already loading or loaded';
     }
-    if (!this.uri_) {
+    if (!this.#uri) {
       throw 'URI for book was not set from loadFromXhr()';
     }
 
-    this.needsLoading_ = false;
+    this.#needsLoading = false;
     this.dispatchEvent(new BookLoadingStartedEvent(this));
 
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', this.uri_, true);
+      xhr.open('GET', this.#uri, true);
       for (const headerKey in headerMap) {
         xhr.setRequestHeader(headerKey, headerMap[headerKey]);
       }
 
       xhr.responseType = 'arraybuffer';
       xhr.onprogress = (evt) => {
-        if (this.bookBinder_) {
+        if (this.#bookBinder) {
           if (expectedSize == -1 && evt.total) {
             expectedSize = evt.total;
-            this.bookBinder_.setNewExpectedSize(evt.loaded, evt.total);
+            this.#bookBinder.setNewExpectedSize(evt.loaded, evt.total);
           }
-          this.dispatchEvent(new BookProgressEvent(this, this.pages_.length));
+          this.dispatchEvent(new BookProgressEvent(this, this.#pages.length));
         }
       };
       xhr.onload = (evt) => {
         const ab = evt.target.response;
-        this.startBookBinding_(this.uri_, ab, expectedSize);
-        this.finishedLoading_ = true;
+        this.#startBookBinding(this.#uri, ab, expectedSize);
+        this.#finishedLoading = true;
         this.dispatchEvent(new BookLoadingCompleteEvent(this));
         resolve(this);
       };
@@ -304,32 +288,32 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>} A Promise that returns this book when all bytes have been fed to it.
    */
   loadFromFetch(expectedSize, init) {
-    if (!this.needsLoading_) {
+    if (!this.#needsLoading) {
       throw 'Cannot try to load via XHR when the Book is already loading or loaded';
     }
-    if (!this.uri_) {
+    if (!this.#uri) {
       throw 'URI for book was not set in loadFromFetch()';
     }
 
-    this.needsLoading_ = false;
+    this.#needsLoading = false;
     this.dispatchEvent(new BookLoadingStartedEvent(this));
 
-    return fetch(this.uri_, init).then(response => {
+    return fetch(this.#uri, init).then(response => {
       const reader = response.body.getReader();
       const readAndProcessNextChunk = () => {
         reader.read().then(({ done, value }) => {
           if (!done) {
             // value is a chunk of the file as a Uint8Array.
-            if (!this.bookBinder_) {
-              return this.startBookBinding_(this.name_, value.buffer, expectedSize).then(() => {
+            if (!this.#bookBinder) {
+              return this.#startBookBinding(this.#name, value.buffer, expectedSize).then(() => {
                 return readAndProcessNextChunk();
               })
             }
-            this.bookBinder_.appendBytes(value.buffer);
+            this.#bookBinder.appendBytes(value.buffer);
             this.appendBytes(value.buffer);
             return readAndProcessNextChunk();
           } else {
-            this.finishedLoading_ = true;
+            this.#finishedLoading = true;
             this.dispatchEvent(new BookLoadingCompleteEvent(this));
             return this;
           }
@@ -346,20 +330,20 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>} A Promise that returns this book when all bytes have been fed to it.
    */
   async loadFromFile() {
-    if (!this.needsLoading_) {
+    if (!this.#needsLoading) {
       throw 'Cannot try to load via File when the Book is already loading or loaded';
     }
-    if (this.uri_) {
+    if (this.#uri) {
       throw 'URI for book was set in loadFromFile()';
     }
-    if (!this.file_ && !this.fileHandle_) {
+    if (!this.#file && !this.#fileHandle) {
       throw 'Neither file nor fileHandle was set inside Book constructor.';
     }
 
     // Set this immediately (before awaiting the file handle) so the ReadingStack does not try
     // to also load it.
-    this.needsLoading_ = false;
-    const file = this.file_ || await this.fileHandle_.getFile();
+    this.#needsLoading = false;
+    const file = this.#file || await this.#fileHandle.getFile();
     this.dispatchEvent(new BookLoadingStartedEvent(this));
 
     return new Promise((resolve, reject) => {
@@ -367,8 +351,8 @@ export class Book extends EventTarget {
       fr.onload = () => {
         const ab = fr.result;
         try {
-          this.startBookBinding_(file.name, ab, ab.byteLength);
-          this.finishedLoading_ = true;
+          this.#startBookBinding(file.name, ab, ab.byteLength);
+          this.#finishedLoading = true;
           this.dispatchEvent(new BookLoadingCompleteEvent(this));
         } catch (err) {
           const errMessage = err + ': ' + file.name;
@@ -387,17 +371,17 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>} A Promise that returns this book when all bytes have been fed to it.
    */
   loadFromArrayBuffer(fileName, ab) {
-    if (!this.needsLoading_) {
+    if (!this.#needsLoading) {
       throw 'Cannot try to load via File when the Book is already loading or loaded';
     }
-    if (this.uri_) {
+    if (this.#uri) {
       throw 'URI for book was set in loadFromArrayBuffer()';
     }
 
-    this.needsLoading_ = false;
+    this.#needsLoading = false;
     this.dispatchEvent(new BookLoadingStartedEvent(this));
-    this.startBookBinding_(fileName, ab, ab.byteLength);
-    this.finishedLoading_ = true;
+    this.#startBookBinding(fileName, ab, ab.byteLength);
+    this.#finishedLoading = true;
     this.dispatchEvent(new BookLoadingCompleteEvent(this));
     return Promise.resolve(this);
   }
@@ -408,14 +392,14 @@ export class Book extends EventTarget {
    * @returns {Promise<Book>} A Promise that returns this book when all bytes have been fed to it.
    */
   loadFromBookPump(bookUri, bookPump) {
-    if (!this.needsLoading_) {
+    if (!this.#needsLoading) {
       throw 'Cannot try to load via BookPump when the Book is already loading or loaded';
     }
-    if (this.uri_) {
+    if (this.#uri) {
       throw 'URI for book was set in loadFromBookPump()';
     }
 
-    this.needsLoading_ = false;
+    this.#needsLoading = false;
     let bookBinderPromise = null;
     return new Promise((resolve, reject) => {
       // If we get any error, reject the promise to create a book.
@@ -425,7 +409,7 @@ export class Book extends EventTarget {
         // If we do not have a book binder yet, create it and start the process.
         if (!bookBinderPromise) {
           try {
-            bookBinderPromise = this.startBookBinding_(bookUri, evt.ab, evt.totalExpectedSize);
+            bookBinderPromise = this.#startBookBinding(bookUri, evt.ab, evt.totalExpectedSize);
           } catch (err) {
             const errMessage = `${err}: ${file.name}`;
             console.error(errMessage);
@@ -436,11 +420,11 @@ export class Book extends EventTarget {
           bookBinderPromise.then(() => {
             switch (evt.type) {
               case BookPumpEventType.BOOKPUMP_DATA_RECEIVED:
-                this.bookBinder_.appendBytes(evt.ab);
+                this.#bookBinder.appendBytes(evt.ab);
                 this.appendBytes(value.buffer);
                 break;
               case BookPumpEventType.BOOKPUMP_END:
-                this.finishedLoading_ = true;
+                this.#finishedLoading = true;
                 this.dispatchEvent(new BookLoadingCompleteEvent(this));
                 resolve(this);
                 break;
@@ -454,18 +438,14 @@ export class Book extends EventTarget {
     });
   }
 
-  /**
-   * @returns {boolean} True if this book has not started loading, false otherwise.
-   */
+  /** @returns {boolean} True if this book has not started loading, false otherwise. */
   needsLoading() {
-    return this.needsLoading_;
+    return this.#needsLoading;
   }
 
-  /**
-   * @param {BookMetata} metadata 
-   */
+  /** @param {BookMetata} metadata */
   setMetadata(metadata) {
-    this.bookMetadata_ = metadata.clone();
+    this.#bookMetadata = metadata.clone();
   }
 
   /**
@@ -476,40 +456,39 @@ export class Book extends EventTarget {
    *                         on which loadFrom... method was called.
    * @param {number} totalExpectedSize
    * @returns {Promise<BookBinder>}
-   * @private
    */
-  startBookBinding_(fileNameOrUri, ab, totalExpectedSize) {
-    this.arrayBuffer_ = ab;
+  #startBookBinding(fileNameOrUri, ab, totalExpectedSize) {
+    this.#arrayBuffer = ab;
     return createBookBinderAsync(fileNameOrUri, ab, totalExpectedSize).then(bookBinder => {
-      this.bookBinder_ = bookBinder;
-      this.bookMetadata_ = createEmptyMetadata(bookBinder.getBookType());
+      this.#bookBinder = bookBinder;
+      this.#bookMetadata = createEmptyMetadata(bookBinder.getBookType());
 
       // Extracts state from some BookBinder events and update the Book. Re-source some of those
       // events, and dispatch them out to the subscribers of this Book. Only some events are
       // propagated from the BookBinder events (those that affect the UI, essentially).
 
-      this.bookBinder_.addEventListener(BookEventType.BINDING_COMPLETE, evt => {
-        this.finishedBinding_ = true;
+      this.#bookBinder.addEventListener(BookEventType.BINDING_COMPLETE, evt => {
+        this.#finishedBinding = true;
         this.dispatchEvent(new BookBindingCompleteEvent(this));
       });
 
-      this.bookBinder_.addEventListener(BookEventType.METADATA_XML_EXTRACTED, evt => {
-        this.bookMetadata_ = evt.bookMetadata;
+      this.#bookBinder.addEventListener(BookEventType.METADATA_XML_EXTRACTED, evt => {
+        this.#bookMetadata = evt.bookMetadata;
       });
 
-      this.bookBinder_.addEventListener(BookEventType.PAGE_EXTRACTED, evt => {
-        this.pages_.push(evt.page);
+      this.#bookBinder.addEventListener(BookEventType.PAGE_EXTRACTED, evt => {
+        this.#pages.push(evt.page);
         this.dispatchEvent(new BookPageExtractedEvent(this, evt.page, evt.pageNum));
       });
 
-      this.bookBinder_.addEventListener(BookEventType.PROGRESS, evt => {
+      this.#bookBinder.addEventListener(BookEventType.PROGRESS, evt => {
         if (evt.totalPages) {
-          this.totalPages_ = evt.totalPages;
+          this.#totalPages = evt.totalPages;
         }
         this.dispatchEvent(new BookProgressEvent(this, evt.totalPages, evt.message));
       });
 
-      this.bookBinder_.start();
+      this.#bookBinder.start();
     });
   }
 }
