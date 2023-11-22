8d2c86fbf8ac59497e7d5baa9718cd46e6d2512a
Jeff Schiller <codedread@gmail.com>
2023-11-21 16:47:36 -0800

    Start on unit tests for TwoPageSetter

--- code/pages/two-page-setter.js
+++ code/pages/two-page-setter.js
@@ -6,7 +6,7 @@ import { PageSetter } from './page-setter.js';
 /** @typedef {import('../book-viewer-types.js').PageLayoutParams} PageLayoutParams */
 /** @typedef {import('../book-viewer-types.js').PageSetting} PageSetting */
 
-// TODO: Add unit tests.
+// TODO: Finish unit tests.
 
 export class TwoPageSetter extends PageSetter {
   /**
